a5302d9586fbfb676027265289367e1be68bab5b
sara-alizadeh-developer <sara.azd89@gmail.com>
2024-05-29 05:26:18 -0700

    first commit

---
+++ Accordion 02/script.js
@@ -0,0 +1,13 @@
+
+const mainItems = document.querySelectorAll(
+    '.main-item'
+  );
+  
+  mainItems.forEach((mainItem) => {
+    mainItem.addEventListener('click', () => {
+      mainItem.classList.toggle(
+        'main-item--open'
+      );
+    })
+  });
+  
